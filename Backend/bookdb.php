098a6e46ce91d1c4f4e8af82655a339ab2b599ee
Youssef Eldoksh <97914775+YoussefEldoksh@users.noreply.github.com>
2025-12-17 19:14:49 +0200

    changed the database from a local server to a cloud server so that everyone in the dev team can use the same version of the database

--- Backend/bookdb.php
+++ Backend/bookdb.php
@@ -1,10 +1,11 @@
 <?php
-$hostName = "localHost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "bookstore";
+$hostName = "sql7.freesqldatabase.com";
+$dbUser = "sql7812236";
+$dbPassword = "R2DRKzTFmz";
+$dbName = "sql7812236";
+$dbPort = "3306";
 
-$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, $dbPort);
 if(!$conn){
     die("Something went wrong!");
 }
